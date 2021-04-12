220622f4375643ed1af54298a1c0bf9b49bce450
boldCM <carolin@cc-meier.de>
2021-04-12 18:20:39 +0200

    feat: create basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Home',
+    component: BottomNavComponent,
+  },
+  {
+    path: 'Einkaufsliste',
+    component: BottomNavComponent,
+  },
+  {
+    path: 'Rezeptübersicht',
+    component: BottomNavComponent,
+  },
+  {
+    path: '',
+    component: BottomNavComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
+import { MainButtonComponent } from './components/bottom-nav/mainButton/main-button/main-button.component';
 
 @NgModule({
-  declarations: [AppComponent, BottomNavComponent],
+  declarations: [AppComponent, BottomNavComponent, MainButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
