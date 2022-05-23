0606adc7e0b66b6a86535a1b514ef75b287dad80
Carolin Meier <carolin.meier@cbc.de>
2022-05-23 16:59:18 +0200

    WIP: recipeList

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RecipeListService } from './recipe-list.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  recipeName:string="KartoffelSalat"
+  public recipeList$: Observable<any>;
 
   // TODO: KalenderIcon hinzufügen
   // TODO: Diamondline kreieren
-  constructor() { }
+  constructor(private readonly recipeListService: RecipeListService) {}
 
   ngOnInit(): void {
+    this.recipeList$ = this.recipeListService.getRecipeList();
   }
-
 }
